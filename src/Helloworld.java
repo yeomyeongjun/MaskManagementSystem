6eba685680ec43032e0a5b41e854e730c2da3d74
USER <USER@SAMSUNG>
2020-03-21 11:00:39 +0900

    create a menu

---
+++ src/Helloworld.java
@@ -0,0 +1,15 @@
+public class Helloworld {
+
+	public static void main(String[] args) {
+		
+		System.out.println(" ***Mask Management Program Menu*** ");
+		System.out.println("==============================");
+		System.out.println("1. Sell Mask ");
+		System.out.println("2. Check for mask distribution");
+		System.out.println("3. Check mask inventory");
+		System.out.println("4. Exit");
+		System.out.println("==============================");
+		System.out.println("Please select one number between 1 - 5");
+		
+	}
+}
